04759535687fbf9d3210c21d5a8a4ab96f29edbc
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-23 19:44:23 +0530

    client setup

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import App from "./App";
+import "./index.css";
+
+const store = configureStore({
+  reducer: {},
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
