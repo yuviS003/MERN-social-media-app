947149ee7198778652c777dd26aeb50e8bbe4ecb
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-24 11:23:59 +0530

    client refractored

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
 
-const store = configureStore({
-  reducer: {},
-});
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
